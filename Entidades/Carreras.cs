c936d24581ad1bceafedc5edefca68fbe0171932
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-01 22:36:39 -0400

    Update Carreras.cs

--- Entidades/Carreras.cs
+++ Entidades/Carreras.cs
@@ -2,19 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace RegistroCarreras.Entidades
+namespace Registro_Estudiantes.Entidades
 {
     public class Carreras
     {
         [Key]
         public int CarreraID { get; set; }
         public string? Nombre { get; set; }
-
+/*
         Carreras (int carreraid, string nombre)
         {
             this.CarreraID = carreraid;
             this.Nombre = nombre;
         }
-
+*/
     }
 }
